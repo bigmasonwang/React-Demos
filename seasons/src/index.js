680f6fb2d2bfe8a0febec715812447a07914e204
mason wang <38519942+bigmasonwang@users.noreply.github.com>
2021-01-04 15:08:16 +1030

    update

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,30 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import SeasonDispplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { lat: null, errorMassage: "" };
+  state = { lat: null, errorMassage: "" };
 
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        this.setState({ lat: position.coords.latitude });
-      },
-      (err) => {
-        this.setState({ errorMassage: err.message });
-      }
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMassage: err.message })
     );
   }
 
-  render() {
+  componentDidUpdate() {
+    console.log("My component was just updated, it rerendered");
+  }
+
+  renderContent() {
     if (this.state.lat && !this.state.errorMassage) {
-      return <div>Latitude: {this.state.lat}</div>;
+      return <SeasonDispplay lat={this.state.lat} />;
     }
     if (!this.state.lat && this.state.errorMassage) {
       return <div>Err: {this.state.errorMassage}</div>;
     }
-    return <div>Loading..</div>;
+    return <Spinner message="please allow the location request" />;
+  }
+
+  render() {
+    return <div className="border red">{this.renderContent()}</div>;
   }
 }
 
