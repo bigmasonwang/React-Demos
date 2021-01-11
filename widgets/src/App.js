0f2251e292f9e26d02fb0eb346abb41ca1fc6f0b
mason wang <38519942+bigmasonwang@users.noreply.github.com>
2021-01-11 18:17:21 +1030

    update

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -38,15 +38,26 @@ const options = [
 
 export default () => {
   const [selected, setSelected] = useState(options[0]);
+  const [showDropdown, setShowDropdown] = useState(true);
+
   return (
     <div>
+      <button
+        onClick={() => {
+          setShowDropdown(!showDropdown);
+        }}
+      >
+        Toggle Dropdown
+      </button>
       {/* <Accordion items={items} /> */}
       {/* <Search /> */}
-      <Dropdown
-        options={options}
-        selected={selected}
-        onSelectedChange={setSelected}
-      />
+      {showDropdown ? (
+        <Dropdown
+          options={options}
+          selected={selected}
+          onSelectedChange={setSelected}
+        />
+      ) : null}
     </div>
   );
 };
