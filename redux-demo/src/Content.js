560a97927b66c06ae778c21ae1853b86879b40dd
mason wang <38519942+bigmasonwang@users.noreply.github.com>
2021-01-22 16:20:20 +1030

    extract connect

--- redux-demo/src/Content.js
+++ redux-demo/src/Content.js
@@ -1,33 +1,25 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import ThemeSwitch from './ThemeSwitch';
+import { connect } from './react-redux';
 
 class Content extends Component {
   static contextTypes = {
-    store: PropTypes.object
+    themeColor: PropTypes.string
   };
-  constructor() {
-    super();
-    this.state = { themeColor: '' };
-  }
-  componentDidMount() {
-    const { store } = this.context;
-    this._updateThemeColor();
-    store.subscribe(() => this._updateThemeColor());
-  }
-  _updateThemeColor() {
-    const { store } = this.context;
-    const state = store.getState();
-    this.setState({ themeColor: state.themeColor });
-  }
   render() {
     return (
       <div>
-        <p style={{ color: this.state.themeColor }}>The Content</p>
+        <p style={{ color: this.props.themeColor }}>The Content</p>
         <ThemeSwitch />
       </div>
     );
   }
 }
-
+const mapStateToProps = (state) => {
+  return {
+    themeColor: state.themeColor
+  };
+};
+Content = connect(mapStateToProps)(Content);
 export default Content;
