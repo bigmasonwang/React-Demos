e256255c5838741ec9c93c5e7b18a1fdff77e971
mason wang <38519942+bigmasonwang@users.noreply.github.com>
2021-01-24 21:56:09 +1030

    optimize use _.memoize

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,13 +1,16 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
-
+import _ from "lodash";
 export const fetchPosts = () => async (dispatch) => {
   const response = await jsonPlaceholder.get("/posts");
 
   dispatch({ type: "FETCH_POSTS", payload: response.data });
 };
 
-export const fetchUser = (id) => async (dispatch) => {
-  const response = await jsonPlaceholder.get(`/users/${id}`);
-
-  dispatch({ type: "FETCH_USER", payload: response.data });
+export const fetchUser = (id) => (dispatch) => {
+  _fetchUser(dispatch, id);
 };
+
+const _fetchUser = _.memoize(async (dispatch, id) => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+  dispatch({ type: "FETCH_USER", payload: response.data });
+});
