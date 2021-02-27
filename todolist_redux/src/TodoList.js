9ec901624e71faaa5b7e513128f59f318211795a
Mason <bigmasonwang@gmail.com>
2021-02-27 20:51:23 +1030

    create store reducer

--- todolist_redux/src/TodoList.js
+++ todolist_redux/src/TodoList.js
@@ -1,28 +1,35 @@
-import React from "react";
-import "antd/dist/antd.css";
-import { Input, Button,List } from "antd";
+import React, { Component } from 'react';
+import 'antd/dist/antd.css';
+import { Input, Button, List } from 'antd';
+import store from './store';
 
-const TodoList = () => {
-  const data = ["Breakfast", "Lunch", "supper"];
-  return (
-    <div style={{ margin: "10px" }}>
-      <div>
-        <Input
-          placeholder="Write Something"
-          style={{ width: "250px", marginRight: "10px" }}
-        ></Input>
-        <Button type="primary">Add</Button>
+class TodoList extends Component {
+  constructor(props) {
+    super(props);
+    // console.log(store.getState());
+    this.state = store.getState();
+  }
+  render() {
+    return (
+      <div style={{ margin: '10px' }}>
+        <div>
+          <Input
+            placeholder={this.state.inputValue}
+            style={{ width: '250px', marginRight: '10px' }}
+          ></Input>
+          <Button type='primary'>Add</Button>
+        </div>
+        <div style={{ margin: '10px', width: '300px' }}>
+          <List
+            size='small'
+            bordered
+            dataSource={this.state.list}
+            renderItem={(item) => <List.Item>{item}</List.Item>}
+          />
+        </div>
       </div>
-      <div style={{ margin: "10px", width: "300px" }}>
-        <List
-          size="small"
-          bordered
-          dataSource={data}
-          renderItem={(item) => <List.Item>{item}</List.Item>}
-        />
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default TodoList;
---
+++ todolist_redux/src/store/reducer.js
@@ -0,0 +1,8 @@
+const defaultState = {
+  inputValue: 'Write Something',
+  list: ['Breakfast', 'Lunch', 'supper'],
+};
+
+export const reducer = (state = defaultState, action) => {
+  return state;
+};
